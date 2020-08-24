047d57d94b91909b1ea37f84dc58a0b89c310796
Michael Levan <mlevan1992@gmail.com>
2020-08-24 16:01:49 -0400

    test updates to build the project resource

---
+++ client/project_groups/resource_project_group.go
@@ -0,0 +1,48 @@
+package project_groups
+
+import (
+	"models"
+	"project_groups"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceProjectGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceProjectGroupCreate,
+		//Read:   resourceProjectGroupRead,
+		//Update: resourceProjectGroupUpdate,
+		//∂Delete: resourceProjectGroupDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func buildProjectGroupResource(d *schema.ResourceData) *project_groups.ClientService {
+	newCreateProjectGroupParams := project_groups.NewCreateProjectGroupParams()
+	createProjectGroupCreated, err := (*project_groups.Client).CreateProjectGroup(newCreateProjectGroupParams, authInfo)
+
+}
+
+func resourceProjectGroupCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*project_groups.Client)
+	name := d.Get("name").(string)
+
+	newProjectGroup := buildProjectGroupResource(d)
+
+	newCreateProjectGroupParams := project_groups.NewCreateProjectGroupParams()
+	newCreateProjectGroupParams.WithProjectGroupResource(&models.ProjectGroupResource{Name: name})
+	createProjectGroupCreated, err := client.ProjectGroups.CreateProjectGroup(newCreateProjectGroupParams, authInfo)
+
+	d.SetId(createProjectGroupCreated)
+	return createProjectGroupCreated.Payload
+}
--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module github.com/OctopusDeploy/terraform-provider-octopusdeploy
 
 require (
 	github.com/OctopusDeploy/go-octopusdeploy v1.6.0
+	github.com/go-openapi/errors v0.19.6
+	github.com/go-openapi/runtime v0.19.20
+	github.com/go-openapi/strfmt v0.19.5
+	github.com/go-openapi/swag v0.19.9
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/terraform v0.12.5
 	github.com/hashicorp/yamux v0.0.0-20180917205041-7221087c3d28 // indirect
 	github.com/mitchellh/gox v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
-	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
-	golang.org/x/sys v0.0.0-20190902133755-9109b7679e13 // indirect
 	gopkg.in/go-playground/validator.v9 v9.21.0 // indirect
 )
 
